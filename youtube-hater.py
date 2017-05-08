db423c801000089268b13c7139b04e17cb1bc1ae
theogoreuxv1 <yodagoreux@gmail.com>
2017-05-08 18:20:26 +0200

    Update youtube-hater.py

--- youtube-hater.py
+++ youtube-hater.py
@@ -1,24 +1,25 @@
 from splinter import *
 from time import sleep
 
-with Browser() as browser:
-	browser.visit("https://accounts.google.com/ServiceLogin")
-	browser.fill("Email", "coucouhelicopter")
-	browser.find_by_id("next").click()
-	browser.fill("Passwd", "jaimelecacao")
-	browser.find_by_id("signIn").click()
+with Browser("chrome") as browser:
+        browser.visit("https://accounts.google.com/ServiceLogin")
+        browser.fill("identifier", "coucouhelicopter")
+        browser.find_by_id("identifierNext").click()
+        sleep(3)
+        browser.fill("password", "jaimelecacao")
+        browser.find_by_id("passwordNext").click()
+        
+        sleep(1)
 	
-	sleep(1)
+        browser.visit("https://youtube.com")
+        browser.find_by_css(".yt-ui-ellipsis").first.click()
+        sleep(5)
+        while True:
 		
-	
-	browser.visit("https://youtube.com")
-	browser.find_by_css(".yt-ui-ellipsis").first.click()
-	
-	sleep(5)
-	
-	while True:
-		
-		
-		browser.find_by_css(".like-button-renderer-dislike-button-unclicked").first.click()
-		browser.find_by_css(".content-link").first.click()
-		sleep(5)
+                try:
+                        browser.find_by_css(".like-button-renderer-like-button-unclicked").first.click()
+                        browser.find_by_css(".content-link").first.click()
+                        sleep(5)
+                except:
+                        browser.find_by_css(".content-link").first.click()
+                        sleep(3)
